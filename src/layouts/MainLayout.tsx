7e36e914a3c864fa89dbe0d90fcdb33e2bc602b6
Nikita Semenov <script696@yandex.ru>
2023-01-26 10:12:15 +0300

    feat: add navigation module

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import s from "./MainLayout.module.scss";
 import { ReactNode } from "react";
+import { Navigation } from "@/modules/navigation";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -8,8 +9,11 @@ type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className={s.mainLayout}>
-      <header className={s.mainLayout__header}>header</header>
-      <nav className={s.mainLayout__navigation}>nav</nav>
+      <div className={s.mainLayout__headerWrapper}>header</div>
+      <div className={s.mainLayout__navWrapper}>
+        {" "}
+        <Navigation />
+      </div>
       <main className={s.mainLayout__main}>{children}</main>
     </div>
   );
---
+++ src/modules/navigation/Navigation.tsx
@@ -0,0 +1,34 @@
+import s from "./styles/Navigation.module.scss";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { NAVIGATION_LINKS } from "@/modules/navigation/constants/constants";
+
+const Navigation = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <nav className={s.navigation}>
+      <h2 className={s.navigation__title}>Меню навигации</h2>
+      <ul className={s.navigation__linksList}>
+        {NAVIGATION_LINKS.map(({ title, path }) => {
+          const isLinkActive = path === pathname;
+
+          return (
+            <li key={path}>
+              <Link
+                href={path}
+                className={
+                  isLinkActive ? s.navigation__link_active : s.navigation__link
+                }
+              >
+                {title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
